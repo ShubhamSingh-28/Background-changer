707b37c4bab329535f69447b446371d1e00b8a59
Shubham <ss9971768704@gmail.com>
2024-02-12 15:05:40 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Background-changer
+# bg-changer
--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import { useState } from "react"
+
+
+
 function App() {
   const [color, setColor] = useState("olive")
 
